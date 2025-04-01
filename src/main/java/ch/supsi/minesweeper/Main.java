96861a629809149f44e33652b4588d6a466506d5
LuanAliprandi <luan.aliprandi@student.supsi.ch>
2025-04-01 11:47:04 +0200

    Iniziato l'implementazione dell metodo create

--- src/main/java/ch/supsi/minesweeper/Main.java
+++ src/main/java/ch/supsi/minesweeper/Main.java
@@ -1,9 +1,12 @@
 package ch.supsi.minesweeper;
 
+import ch.supsi.minesweeper.model.ReadWriteUtilities;
+
 public class Main {
 
     public static void main( String[] args ) {
-        MainFx.main(args);
+        //MainFx.main(args);
+        ReadWriteUtilities.create();
     }
 
 }
--- src/main/java/ch/supsi/minesweeper/model/ReadWriteUtilities.java
+++ src/main/java/ch/supsi/minesweeper/model/ReadWriteUtilities.java
@@ -1,4 +1,36 @@
 package ch.supsi.minesweeper.model;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Properties;
+
 public class ReadWriteUtilities {
+    private static final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+    public static Properties read(File file) {
+        return null;
+    }
+
+    public static void write(File file) {
+
+    }
+
+    public static void create() {
+        File dir = new File(System.getProperty("user.home")+"\\minesweeper");
+
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+/*
+
+        try {
+            Files.copy(saves.toPath(), dir.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(settings.toPath(), dir.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+*/
+    }
 }
