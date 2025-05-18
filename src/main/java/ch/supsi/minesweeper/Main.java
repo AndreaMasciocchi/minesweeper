08e23b341880c5d7f907929852670b9124392a1a
LuanAliprandi <luan.aliprandi@student.supsi.ch>
2025-05-18 10:30:25 +0200

    Aggiunti it file di traduzione

--- src/main/java/ch/supsi/minesweeper/Main.java
+++ src/main/java/ch/supsi/minesweeper/Main.java
@@ -1,12 +1,6 @@
 package ch.supsi.minesweeper;
 
 import ch.supsi.minesweeper.controller.UserPreferencesController;
-import ch.supsi.minesweeper.dataaccess.UserPreferencePropertiesDao;
-import ch.supsi.minesweeper.model.PreferencesDataAccessInterface;
-import ch.supsi.minesweeper.model.ReadWriteUtilities;
-
-import java.io.File;
-import java.util.Properties;
 
 public class Main {
 
